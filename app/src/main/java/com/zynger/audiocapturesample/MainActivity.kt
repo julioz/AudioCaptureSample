4d6cb7ba2a9a1f2851084662c170155ab378fc0e
Julio Zynger <julio.zynger@soundcloud.com>
2019-09-10 11:29:16 +0200

    Handle runtime permission request

--- app/src/main/java/com/zynger/audiocapturesample/MainActivity.kt
+++ app/src/main/java/com/zynger/audiocapturesample/MainActivity.kt
@@ -1,12 +1,71 @@
 package com.zynger.audiocapturesample
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.btn_start_recording)
+            .setOnClickListener {
+                startCapturing()
+            }
+    }
+
+    private fun startCapturing() {
+        if (!isRecordAudioPermissionGranted()) {
+            requestRecordAudioPermission()
+        } else {
+            // capture audio
+        }
+    }
+
+    private fun isRecordAudioPermissionGranted(): Boolean {
+        return ContextCompat.checkSelfPermission(
+            this,
+            Manifest.permission.RECORD_AUDIO
+        ) == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun requestRecordAudioPermission() {
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(Manifest.permission.RECORD_AUDIO),
+            RECORD_AUDIO_PERMISSION_REQUEST_CODE
+        )
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == RECORD_AUDIO_PERMISSION_REQUEST_CODE) {
+            if (grantResults.firstOrNull() == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(
+                    this,
+                    "Permissions to capture audio granted. Click the button once again.",
+                    Toast.LENGTH_LONG
+                ).show()
+            } else {
+                Toast.makeText(
+                    this, "Permissions to capture audio denied.",
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+        }
+    }
+
+    companion object {
+        private const val RECORD_AUDIO_PERMISSION_REQUEST_CODE = 42
     }
 }
